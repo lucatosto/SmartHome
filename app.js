70d4f0a0104938d3194845a5fde3e2abaf5ece7b
Luca Tosto <lucatosto1391@gmail.com>
2017-06-20 12:59:06 +0200

    port setting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3100;
+const port = $PORT;
 
 http.listen(port, function(){
     console.log('listening on *:'+port);

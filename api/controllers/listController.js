7a7d1f3b15b0a92d1ebad0e925790e0a0e26882b
Luca Tosto <lucatosto1391@gmail.com>
2017-06-22 12:01:37 +0200

    add mqtt publish

--- api/controllers/listController.js
+++ api/controllers/listController.js
@@ -55,8 +55,57 @@ exports.delete_a_task = function(req, res) {
 
 exports.test_communication = function(req, res){
 
-    res.json({message: "U sucu do pollu!"})
+    res.json(
+      {message: "U sucu do pollu!"}
+    );
 
-    console.log("Ho ricevuto questo: \n")
-    console.log(req.body);
+    var mqtt = require('mqtt'), url = require('url');
+    var url = "mqtt://" + "m12.cloudmqtt.com";
+
+
+    //https://www.cloudmqtt.com/
+    //email: aikon_89@hotmail.it
+    //pass: sistemiembedded
+    var options = {
+      port: 11914,
+      clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+      username: 'yigqtcwy',
+      password: 'cqnOzCX8sbff'
+    };
+
+
+    var my_message = 'U sucu do pollu'; //my command
+    var pub_topic = 'cmd/actuator/on/';
+    var rcv_topic = 'receive/actuator';
+
+    pub_topic=rcv_topic;
+
+    // Create a client connection
+    var client = mqtt.connect(url, options);
+    try {
+    	console.log('Client mqtt create;\n');
+    	client.on('connect', function() { // When connected
+    	console.log('connect;\n');
+
+    	// subscribe to a topic
+    	  client.subscribe(pub_topic, function() {
+    	    // when a message arrives, do something with it
+    	    client.on('message', function(topic, message, packet) {
+    	      console.log("Received '" + message + "' on '" + topic + "'");
+    	    });
+    	  });
+
+    	// publish a message to a topic
+    	  client.publish(pub_topic, my_message +' '+ new Date().toString(), function() {
+    	    console.log("Message is published");
+    	    //client.end(); // Close the connection when published
+    	  });
+    	});
+
+
+
+        } catch (err) {
+        	console.log(err)
+    }
+    console.log("Ho inviato tutto su cloud mqtt. ")
 };
